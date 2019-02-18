89df5e8c288b74508c6b29759ccda454537a5e35
Bacmel <hugo.duarte91800@gmail.com>
2019-02-18 22:29:27 +0100

    MAJ projet.c

--- projet.c
+++ projet.c
@@ -10,6 +10,7 @@
 #include <assert.h>
 #include "graph.h"
 #include "tas.h"
+#include "prim.h"
 
 /********/
 /* main */
@@ -18,32 +19,21 @@
 /* le contenu du fichier main est donne ici a titre de demonstration */
 /* du fonctionnement des differentes fonctions et procedures */
 int main(int argc, char *argv[]) {
+    srand(time(NULL));
     Graphe g;
     Coordonnees c;
-    Tas t;
 
     /* lecture de l'instance */
     c = lecture_instance(argv[1]);
-
+    g = prim(c);
     /* affichage de l'instance */
     //afficher_instance(argv[1]);
-
+    afficher_graphe(c, g);
     /* affichage de la tournee optimale */
     //char nom_tour[256];
     //sprintf(nom_tour,"%s.opt",argv[1]);
     //afficher_tour(c,nom_tour);
-    g = creer_graphe(c->n);
-    t = creer_tas(c->n);
-
-    int i;
-    for (i = 0; i < 10; i++) {
-        tas_inserer(t, i, c);
-        tas_afficher(t);
-    }
-    while (tas_taille(t) != 0) {
-        printf("tete : %d\n", tas_retirer_tete(t));
-        //tas_afficher(t);
-    }
+    //g = creer_graphe(c->n);
     /* toutes les aretes partant du sommet 0 sont presentes */
     /* Dummy test
     int i,j;
@@ -64,8 +54,6 @@ int main(int argc, char *argv[]) {
     /* liberation de la memoire */
     detruire_graphe(g);
     detruire_coordonnees(c);
-    detruire_tas(&t);
-
     return 0;
 }
 
