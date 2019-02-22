20cf341baad2c74c1b2531607a57b2b2fff61865
Florian Cormee <florian.cormee@gmail.com>
2019-02-22 16:08:42 +0100

    poid pi implemente

---
+++ poid_pi.c
@@ -0,0 +1,5 @@
+//
+// Created by florian on 22/02/19.
+//
+
+#include "poid_pi.h"
---
+++ poid_pi.h
@@ -0,0 +1,8 @@
+//
+// Created by florian on 22/02/19.
+//
+
+#ifndef INFORMATIQUE_POID_PI_H
+#define INFORMATIQUE_POID_PI_H
+
+#endif //INFORMATIQUE_POID_PI_H
